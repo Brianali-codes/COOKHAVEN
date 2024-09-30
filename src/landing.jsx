871140a69df4c9519dbad21c2dfe67b664db36b0
Brian Ali <gamerfunky78@gmail.com>
2024-09-30 18:48:43 +0300

    Update landing.jsx

--- src/landing.jsx
+++ src/landing.jsx
@@ -13,7 +13,7 @@ function LandingPage(){
             <div className="content-overlay w-4/6 p-2 flex flex-col justify-end">
                 <h1 className="h11 text-start">COOK HAVEN</h1>
                 <br />
-                <p className='h9 text-start'>Welcome to RecipeBible – Your Home for Delicious Recipes! Discover the joy of cooking with our collection of easy-to-follow, flavorful recipes designed for every occasion. Whether you’re a seasoned chef or a kitchen beginner, our step-by-step guides make meal preparation simple, fun, and stress-free. From savory dinners to indulgent desserts, healthy bites to quick snacks, we’ve got something for everyone. Get inspired, try something new, and enjoy the taste of homemade meals made with love!</p>
+                <p className='h9 text-start'>Welcome to CookHaven – Your Home for Delicious Recipes! Discover the joy of cooking with our collection of easy-to-follow, flavorful recipes designed for every occasion. Whether you’re a seasoned chef or a kitchen beginner, our step-by-step guides make meal preparation simple, fun, and stress-free. From savory dinners to indulgent desserts, healthy bites to quick snacks, we’ve got something for everyone. Get inspired, try something new, and enjoy the taste of homemade meals made with love!</p>
             </div>
          </div>
         )
